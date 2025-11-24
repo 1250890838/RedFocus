adf8f02178743eaeb02acd42cc4df73c319b9952
fangyefeng <1250890838@qq.com>
2025-11-24 23:02:08 +0800

    Add TimerConfig and ViewModelBase classes
    Introduces the `TimerConfig` class in the `RedFocus.Model` namespace for timer configuration, including properties for focus and break times. Also adds the `ViewModelBase` class in the `RedFocus.ViewModel` namespace, implementing `INotifyPropertyChanged` for property change notifications and management.

---
+++ Model/TimerConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedFocus.Model;
+public class TimerConfig
+{
+    public TimeSpan FocusTime { get; set; }
+    public TimeSpan ShortBreakTime { get; set; }
+    public TimeSpan LongBreakTime { get; set; }
+    public int Rounds { get; set; } = 0;
+}
