7b29397696dec4e00fbb4517bf0144107eb66a29
fangyefeng <1250890838@qq.com>
2025-11-25 22:44:23 +0800

    Add SwitchButton and TimerViewModel enhancements
    In MainWindow.xaml, removed TotalSeconds and Margin properties, and added a new SwitchButton control bound to CountdownTimer's IsRunning property with styling attributes.
    
    In TimerViewModel.cs, introduced TimerState enum and TimerViewModel class with properties for managing timer states and durations, including property change notifications.

---
+++ ViewModel/TimerViewModel.cs
@@ -0,0 +1,47 @@
+﻿using RedFocus.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace RedFocus.ViewModel;
+public enum TimerState
+{
+    Focus,
+    ShortBreak,
+    LongBreak
+}
+public class TimerViewModel : ViewModelBase
+{
+    public TimerConfigViewModel TimerConfig { get; init; }
+    public TimerViewModel(TimerConfigViewModel timerConfig)
+    {
+        TimerConfig = timerConfig;
+        CurrentState = TimerState.Focus;
+    }
+
+    private TimerState _currentState;
+    public TimerState CurrentState
+    {
+        get => _currentState;
+        set
+        {
+            _currentState = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public TimeSpan CurrentTime
+    {
+        get
+        {
+            return CurrentState switch
+            {
+                TimerState.Focus => TimerConfig.FocusTime,
+                TimerState.ShortBreak => TimerConfig.ShortBreakTime,
+                TimerState.LongBreak => TimerConfig.LongBreakTime,
+                _ => TimeSpan.Zero
+            };
+        }
+    }
+}
