cefe6ba797fece86609495e5cbdfc7ed8ae38ee9
fyf <1250890838@qq.com>
2025-11-25 20:00:20 +0800

    添加计时器配置的双向绑定支持
    重构 `TimeSpanToDoubleConverter` 类为顶级类，并新增 `IntToTimeSpanConverter` 类以支持 `int` 和 `TimeSpan` 的双向转换。
    
    在 `MainWindow.xaml` 中设置了 `TimerConfigViewModel` 作为数据上下文。
    
    在 `TimerConfigurationPage.xaml` 中：
    - 添加了 `IntToTimeSpanConverter` 资源。
    - 为多个 `TimeIntervalSlider` 控件绑定了 `TimerConfigViewModel` 的属性。
    - 为 `RoundsSlider` 添加了双向绑定支持。
    
    新增 `TimerConfigViewModel` 类以管理计时器配置，支持属性验证、重置命令和配置导入/导出功能。

--- Converters/TimeSpanToDoubleConverter.cs
+++ Converters/TimeSpanToDoubleConverter.cs
@@ -4,19 +4,29 @@ using System.Globalization;
 using System.Text;
 using System.Windows.Data;
 
-namespace RedFocus.Converters
-{
-    public class TimeSpanToDoubleConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            return value is TimeSpan ts ? ts.TotalMinutes : 0;
-        }
+namespace RedFocus.Converters;
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            return TimeSpan.FromMinutes((double)value);
-        }
+public class TimeSpanToDoubleConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value is TimeSpan ts ? ts.TotalMinutes : 0;
     }
 
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return TimeSpan.FromMinutes((double)value);
+    }
+}
+
+public class IntToTimeSpanConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value is int minutes ? TimeSpan.FromMinutes(minutes) : TimeSpan.Zero;
+    }
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value is TimeSpan ts ? (int)ts.TotalMinutes : 0;
+    }
 }
---
+++ ViewModel/TimerConfigViewModel.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Windows.Input;
+using RedFocus.Model;
+
+namespace RedFocus.ViewModel;
+public class TimerConfigViewModel : ViewModelBase
+{
+    private TimerConfig _timerConfig = new()
+    {
+        FocusTime = TimeSpan.FromMinutes(25),
+        ShortBreakTime = TimeSpan.FromMinutes(5),
+        LongBreakTime = TimeSpan.FromMinutes(15),
+        Rounds = 4
+    };
+    public TimeSpan FocusTime
+    {
+        get => _timerConfig.FocusTime;
+        set
+        {
+            var v = value <= TimeSpan.Zero ? TimeSpan.FromMinutes(1) : value;
+            if (_timerConfig.FocusTime != v)
+            {
+                _timerConfig.FocusTime = v;
+                OnPropertyChanged();
+            }
+        }
+    }
+    public TimeSpan ShortBreakTime
+    {
+        get => _timerConfig.ShortBreakTime;
+        set
+        {
+            var v = value <= TimeSpan.Zero ? TimeSpan.FromMinutes(1) : value;
+            if (_timerConfig.ShortBreakTime != v)
+            {
+                _timerConfig.ShortBreakTime = v;
+                OnPropertyChanged();
+            }
+        }
+    }
+    public TimeSpan LongBreakTime
+    {
+        get => _timerConfig.LongBreakTime;
+        set
+        {
+            var v = value <= TimeSpan.Zero ? TimeSpan.FromMinutes(1) : value;
+            if (_timerConfig.LongBreakTime != v)
+            {
+                _timerConfig.LongBreakTime = v;
+                OnPropertyChanged();
+            }
+        }
+    }
+    public int Rounds
+    {
+        get => _timerConfig.Rounds;
+        set
+        {
+            var v = value < 1 ? 1 : value;
+            if (_timerConfig.Rounds != v)
+            {
+                _timerConfig.Rounds = v;
+                OnPropertyChanged();
+            }
+        }
+    }
+    public ICommand ResetCommand { get; }
+    public TimerConfigViewModel()
+    {
+        ResetCommand = new RelayCommand(_ => ResetToDefault());
+    }
+    public void Load(TimerConfig config)
+    {
+        _timerConfig = config;
+        OnPropertyChanged(nameof(FocusTime));
+        OnPropertyChanged(nameof(ShortBreakTime));
+        OnPropertyChanged(nameof(LongBreakTime));
+        OnPropertyChanged(nameof(Rounds));
+    }
+    public TimerConfig Export() => _timerConfig;
+    private void ResetToDefault()
+    {
+        Load(new TimerConfig
+        {
+            FocusTime = TimeSpan.FromMinutes(25),
+            ShortBreakTime = TimeSpan.FromMinutes(5),
+            LongBreakTime = TimeSpan.FromMinutes(15),
+            Rounds = 4
+        });
+    }
+    private class RelayCommand : ICommand
+    {
+        private readonly Action<object?> _execute;
+        private readonly Func<object?, bool>? _canExecute;
+        public RelayCommand(Action<object?> execute, Func<object?, bool>? canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+        public bool CanExecute(object? parameter) => _canExecute?.Invoke(parameter) ?? true;
+        public void Execute(object? parameter) => _execute(parameter);
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+        public void RaiseCanExecuteChanged() => CommandManager.InvalidateRequerySuggested();
+    }
+}
