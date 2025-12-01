ed74e72fae8630ac12343dac47508f47b5b1f24f
fyf <1250890838@qq.com>
2025-12-01 19:59:19 +0800

    新增 ColorItem 控件及调整 ThemesPage 布局
    在 ColorItem.cs 中新增自定义控件 ColorItem，定义了多个依赖属性（AccentColor、PrimaryColor、Text、IsSelected），并重写了默认样式元数据。
    
    在 ThemesPage.xaml 中：
    - 添加了对 ColorItem 控件的样式支持。
    - 修改页面标题为 "Themes"，调整字体样式。
    - 将布局从水平排列改为垂直排列，删除了部分无关文本。

---
+++ Controls/ColorItem.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RedFocus.Controls;
+public class ColorItem : Control
+{
+    static ColorItem()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(ColorItem), new FrameworkPropertyMetadata(typeof(ColorItem)));
+    }
+
+    #region 依赖属性
+    static readonly DependencyProperty AccentColorProperty =
+        DependencyProperty.Register(nameof(AccentColor), typeof(Color), typeof(ColorItem),
+            new PropertyMetadata(Colors.Transparent));
+    public Color AccentColor
+    {
+        get => (Color)GetValue(AccentColorProperty);
+        set => SetValue(AccentColorProperty, value);
+    }
+
+    static readonly DependencyProperty PrimaryColorProperty =
+        DependencyProperty.Register(nameof(PrimaryColor), typeof(Color), typeof(ColorItem),
+            new PropertyMetadata(Colors.Transparent));
+    public Color PrimaryColor
+    {
+        get => (Color)GetValue(PrimaryColorProperty);
+        set => SetValue(PrimaryColorProperty, value);
+    }
+
+    static readonly DependencyProperty TextProperty =
+        DependencyProperty.Register(nameof(Text), typeof(string), typeof(ColorItem),
+            new PropertyMetadata(false));
+    public string Text
+    {
+        get => (string)GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+    static readonly DependencyProperty IsSelectedProperty =
+        DependencyProperty.Register(nameof(IsSelected), typeof(bool), typeof(ColorItem),
+            new PropertyMetadata(false));
+    public bool IsSelected
+    {
+        get => (bool)GetValue(IsSelectedProperty);
+        set => SetValue(IsSelectedProperty, value);
+    }
+
+    #endregion
+}
